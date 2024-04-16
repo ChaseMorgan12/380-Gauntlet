6c7fcee6ab123c6fd14182f59fcb29a08c2e0cb6
Chase <129904811+ChaseMorgan12@users.noreply.github.com>
2024-04-16 13:48:40 -0700

    Push to my desktop

--- Gauntlet Clone/Assets/Scripts/PlayerController.cs
+++ Gauntlet Clone/Assets/Scripts/PlayerController.cs
@@ -13,8 +13,30 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float playerSpeed = 5.0f;
 
+    private Vector2 moveValue;
+
+    private void Update()
+    {
+        transform.Translate(playerSpeed * Time.deltaTime * new Vector3(moveValue.x, 0, moveValue.y));
+    }
+
     public void Move(InputAction.CallbackContext context)
     {
+        moveValue = context.ReadValue<Vector2>();
+    }
 
+    public void Attack1(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack 1");
+    }
+
+    public void Attack2(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack 2");
+    }
+
+    public void Attack3(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack 3");
     }
 }
