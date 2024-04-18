53977e4c35c618c66a862187cd4fae4466ef999a
ConnerZepeda <129878540+ConnerZepeda@users.noreply.github.com>
2024-04-18 11:14:44 -0700

    Making basic test enemy, moves towards player if in range

--- Gauntlet Clone/Assets/Scripts/BaseEnemy.cs
+++ Gauntlet Clone/Assets/Scripts/BaseEnemy.cs
@@ -10,10 +10,10 @@ using UnityEngine;
 
 public abstract class BaseEnemy : MonoBehaviour, IDamageable
 {
-    protected float damage, speed, attackRange, attackCooldown;
+    protected float damage, speed, attackRange, attackCooldown, detectionRange, moveRange;
 
     float IDamageable.Health { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-
+    
     public virtual void Attack1()
     {
 
@@ -23,7 +23,19 @@ public abstract class BaseEnemy : MonoBehaviour, IDamageable
     {
 
     }
+    public virtual void MoveTowardsPlayer()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRange);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            if (hitColliders[i].CompareTag("Player"))
+            {
+                Debug.Log("Player detected");
+                transform.position = Vector3.MoveTowards(transform.position, hitColliders[i].transform.position, moveRange);
+            }
 
+        }
+    }
     public void Damage(float amount)
     {
         throw new System.NotImplementedException();
