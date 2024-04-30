0052314a2229edf41cfceec14b578b841cf21582
Chase <129904811+ChaseMorgan12@users.noreply.github.com>
2024-04-30 13:17:03 -0700

    Finished implementation

--- Gauntlet Clone/Assets/Scripts/Generator.cs
+++ Gauntlet Clone/Assets/Scripts/Generator.cs
@@ -13,6 +13,9 @@ public class Generator : Damageable
     //Fields
     [SerializeField] private GameObject enemy;
     [SerializeField, Range(0.25f, 30f)] private float spawnInterval = 1f;
+    [SerializeField, Range(1, 100)] private int maxSpawnAmount = 25;
+
+    private readonly List<GameObject> enemies = new();
 
     private void Start()
     {
@@ -23,9 +26,26 @@ public class Generator : Damageable
     {
         yield return new WaitForSeconds(spawnInterval);
 
-        Vector3 spawnPos = transform.position + (transform.forward * transform.localScale.magnitude + new Vector3(Random.Range(-.500f, .500f), 0, Random.Range(-.500f, .500f)));
+        if (enemies.Count < maxSpawnAmount)
+        {
+            Vector3 spawnPos = transform.position + (transform.forward * transform.localScale.magnitude + new Vector3(Random.Range(-.500f, .500f), 0, Random.Range(-.500f, .500f)));
 
-        GameObject spawnedEnemy = Instantiate(enemy, spawnPos, Quaternion.identity);
+            GameObject spawnedEnemy = Instantiate(enemy, spawnPos, Quaternion.identity);
+            enemies.Add(spawnedEnemy);
+        }
+        else
+        {
+            //Clear up space so we can spawn more later
+            List<GameObject> enemiesToRemove = new();
+            foreach(GameObject enemy in enemies)
+            {
+                if (enemy == null)
+                    enemiesToRemove.Add(enemy);
+            }
+
+            foreach (GameObject enemy in enemiesToRemove)
+                enemies.Remove(enemy);
+        }
 
         StartCoroutine(Spawn());
     }
@@ -39,11 +59,4 @@ public class Generator : Damageable
             Destroy(gameObject);
         }
     }
-
-    /*TESTING ONLY
-    private void OnGUI()
-    {
-        if (GUILayout.Button("Damage Generator"))
-            Damage(10);
-    } */
 }
