ab32423bb0144748c8993b896f87b0a8eeab6976
ChaseMorgan12 <chasewmorgan12@gmail.com>
2024-05-14 12:55:24 -0700

    Built

--- Gauntlet Clone/Assets/Scripts/EnemyClasses/Demon.cs
+++ Gauntlet Clone/Assets/Scripts/EnemyClasses/Demon.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 /* FILE HEADER
 *  Edited by: Chase Morgan, Conner Zepeda
--- Gauntlet Clone/Assets/Scripts/EnemyClasses/Sorcerer.cs
+++ Gauntlet Clone/Assets/Scripts/EnemyClasses/Sorcerer.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 /* FILE HEADER
 *  Edited by: Chase Morgan, Conner Zepeda
